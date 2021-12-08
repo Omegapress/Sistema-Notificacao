1a2ef75ae71632b2b3dbae521a3e71544496678a
fabricio <fabricio.correa.abreu@gmail.com>
2021-12-08 19:05:53 -0200

    Adicionar arquivos de projeto.

---
+++ Aplicacao de envio de mensagens/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaNotificacao
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("*** Sistema de notificação de clientes 1.0***");
+            Console.WriteLine("Informe código do cliente");
+            var CodCliente = Console.ReadLine();
+            Console.WriteLine("Informe mensagem a ser enviada: ");
+            var Mensagem = Console.ReadLine();
+
+            //Simula envio de um email
+
+            Console.WriteLine("Enviado EMAIL para o cliente {0}", CodCliente);
+            Console.WriteLine("EMAIL: {0}", Mensagem);
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
