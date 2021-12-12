c80d42ce5f6ea452b44d2bc8a221d8caac57fc5d
Fabricio Abreu <fabricio.abreu_02@outlook.com>
2021-12-11 21:19:26 -0300

    Atualização do Sistema com funcionalidades de envio por SMS e VoiceEmail

--- Aplicacao de envio de mensagens/Program.cs
+++ Aplicacao de envio de mensagens/Program.cs
@@ -10,17 +10,46 @@ namespace SistemaNotificacao
 
         static void Main(string[] args)
         {
+            //Inicio do Programa de notificação de Cliente
 
-            Console.WriteLine("*** Sistema de notificação de clientes 1.0***");
+            Console.WriteLine("*** Sistema de notificação de clientes 1.1***");
             Console.WriteLine("Informe código do cliente");
             var CodCliente = Console.ReadLine();
             Console.WriteLine("Informe mensagem a ser enviada: ");
             var Mensagem = Console.ReadLine();
+            Console.WriteLine("Digite a opção de Envio 1 - EMAIL , 2 - SMS , 3 - VOICE");
+            var modoEnvio = Console.ReadLine();
 
-            //Simula envio de um email
+            if (modoEnvio == "1")
+            {
 
-            Console.WriteLine("Enviado EMAIL para o cliente {0}", CodCliente);
-            Console.WriteLine("EMAIL: {0}", Mensagem);
+                //SIMULA O ENVIO NO MODO EMAIL
+
+                Console.WriteLine("Enviado EMAIL para o cliente {0}", CodCliente);
+                Console.WriteLine("EMAIL: {0}", Mensagem);               
+
+            }
+
+            else if (modoEnvio == "2")
+
+            {
+
+                //SIMULA O ENVIO NO MODO SMS
+
+                Console.WriteLine("Enviado SMS para o cliente {0}", CodCliente);
+                Console.WriteLine("SMS: {0}", Mensagem);
+
+            }
+
+            else if (modoEnvio == "3")
+            {
+                //SIMULA O ENVIO NO MODO VOICE
+
+                Console.WriteLine("Enviado VOICEMAIL para o cliente {0}", CodCliente);
+                Console.WriteLine("VOICEMAIL: {0}", Mensagem);
+
+            }
+
             Console.ReadLine();
 
         }
